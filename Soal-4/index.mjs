3a8fc1a73efddd2f9aeff7983aaf655da8d075c0
rizkyalvie <rizkyalviee@gmail.com>
2022-07-14 03:13:34 +0700

    CRUD Kabupaten

--- Soal-4/index.mjs
+++ Soal-4/index.mjs
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: false }))
 
 app.set('view engine', 'hbs')
 
-
+// get 
 app.get('/', (req, res) => {
 
     const sql = "SELECT * FROM provinsi_tb";
@@ -43,37 +43,80 @@ app.get('/', (req, res) => {
         return data
       })
 
-      res.render('index', {provinsi: fetchProvinsi})
-    });
+      const sql2 = "SELECT * FROM kabupaten_tb";
 
-})
+    db.query(sql2, function (err, result) {
+      if (err) throw err;;
 
-app.get('/add-province', (req, res) => {
-    res.render('addprovince')
-})
+      const dataKabupaten = result
 
-app.post('/addprovince', upload.single('photo'), (req, res) => {
-
-    let nama = req.body.nama
-    let diresmikan = req.body.diresmikan
-    let photo = req.file.filename
-    let pulau = req.body.pulau
-
-    const sql = `INSERT INTO provinsi_tb (nama, diresmikan, photo, pulau) VALUES ('${nama}', '${diresmikan}', '${photo}', '${pulau}');`;
-
-    db.query(sql, function (err, result) {
-      if (err) throw err;
-
-      res.redirect('/')
+      const fetchKabupaten = dataKabupaten.map((data) => {
+        data.photo = '/uploads/' + data.photo
+        return data
       })
 
+      res.render('index', {provinsi: fetchProvinsi, kabupaten: fetchKabupaten})
+    });
+    
+    })
+})
+
+app.get('/province-detail/:id', (req, res) => {
+    let id = req.params.id
+
+    const sql = `SELECT * FROM provinsi_tb WHERE provinsi_tb.id=${id}`;
+
+    db.query(sql, function (err, result) {
+        if (err) throw err;
+  
+        console.log(`ID \t NAME \t Diresmikan \t Pulau `);
+        console.log(`----------------------------------------------------------`);
+        result.forEach((data) => {
+          console.log(`${data.id} \t ${data.nama} \t ${data.diresmikan} \t ${data.pulau}`);
+        });
+  
+        console.log(`----------------------------------------------------------`);
+        result.forEach((data) => {
+          console.log(`${data.nama} -> ${data.pulau}`);
+        });
+  
+        const dataProvinsi = result
+
+        const fetchProvinsi = dataProvinsi.map((data) => {
+            data.photo = 'http://localhost:8080/uploads/' + data.photo
+
+            return data
+        })
+  
+        res.render('detail', {provinsi: fetchProvinsi, id: id})
+      });
+})
+
+app.get('/city-detail/:id', (req, res) => {
+    let id = req.params.id
+
+    const sql = `SELECT * FROM kabupaten_tb WHERE kabupaten_tb.id=${id}`;
+
+    db.query(sql, function (err, result) {
+        if (err) throw err;
+  
+        const dataKabupaten = result
+
+        const fetchKabupaten = dataKabupaten.map((data) => {
+            data.photo = 'http://localhost:8080/uploads/' + data.photo
+
+            return data
+        })
+  
+        res.render('citydetail', {kabupaten: fetchKabupaten, id: id})
+      });
 })
 
 app.get('/update-province/:id', (req, res) => {
 
     let id = req.params.id
 
-    const sql = `SELECT * FROM provinsi_tb WHERE id=${id}`;
+    const sql = `SELECT * FROM provinsi_tb WHERE provinsi_tb.id=${id}`;
 
     db.query(sql, function (err, result) {
         if (err) throw err;
@@ -91,14 +134,105 @@ app.get('/update-province/:id', (req, res) => {
   
         const dataProvinsi = result
   
-        const fetchProvinsi = dataProvinsi.map((data) => {
-          data.photo = '/uploads/' + data.photo
-          return data
-        })
-  
-        res.render('updateprovince', {provinsi: fetchProvinsi, id: id})
+        res.render('updateprovince', {provinsi: dataProvinsi, id: id})
       });
   
+})
+
+app.get('/update-city/:id', (req, res) => {
+
+    let id = req.params.id
+
+    const sql = `SELECT * FROM kabupaten_tb WHERE kabupaten_tb.id=${id}`;
+
+    db.query(sql, function (err, result) {
+        if (err) throw err;
+  
+        const dataKabupaten = result
+  
+        res.render('updatecity', {kabupaten: dataKabupaten, id: id})
+      });
+  
+})
+
+app.get('/add-province', (req, res) => {
+    res.render('addprovince')
+})
+
+app.get('/add-city', (req, res) => {
+    const sql = "SELECT * FROM provinsi_tb";
+
+    db.query(sql, function (err, result) {
+      if (err) throw err;
+
+      const dataProvinsi = result
+
+      const fetchProvinsi = dataProvinsi.map((data) => {
+        data.photo = '/uploads/' + data.photo
+        return data
+      })
+
+      res.render('addcity', {provinsi: fetchProvinsi})
+    });
+})
+
+app.get('/delete-provinsi/:id', (req, res) => {
+
+    const id = req.params.id
+
+    const sql = `DELETE FROM provinsi_tb WHERE provinsi_tb.id=${id}`;
+    db.query(sql, function (err, result) {
+        if (err) throw err;
+  
+        res.redirect('/')
+        })
+})
+
+app.get('/delete-city/:id', (req, res) => {
+
+    const id = req.params.id
+
+    const sql = `DELETE FROM kabupaten_tb WHERE kabupaten_tb.id=${id}`;
+    db.query(sql, function (err, result) {
+        if (err) throw err;
+  
+        res.redirect('/')
+        })
+})
+
+// post 
+app.post('/addprovince', upload.single('photo'), (req, res) => {
+
+    let nama = req.body.nama
+    let diresmikan = req.body.diresmikan
+    let photo = req.file.filename
+    let pulau = req.body.pulau
+
+    const sql = `INSERT INTO provinsi_tb (nama, diresmikan, photo, pulau) VALUES ('${nama}', '${diresmikan}', '${photo}', '${pulau}');`;
+
+    db.query(sql, function (err, result) {
+      if (err) throw err;
+
+      res.redirect('/')
+      })
+
+})
+
+app.post('/addcity', upload.single('photo'), (req, res) => {
+
+    let nama = req.body.nama
+    let diresmikan = req.body.diresmikan
+    let photo = req.file.filename
+    let provinsi = req.body.provinsi
+
+    const sql = `INSERT INTO kabupaten_tb (nama, diresmikan, photo, provinsi_id) VALUES ('${nama}', '${diresmikan}', '${photo}', '${provinsi}');`;
+
+    db.query(sql, function (err, result) {
+      if (err) throw err;
+
+      res.redirect('/')
+      })
+
 })
 
 app.post('/updateprovince/:id', upload.single('photo'), (req, res) => {
@@ -120,6 +254,24 @@ app.post('/updateprovince/:id', upload.single('photo'), (req, res) => {
 
 })
 
+app.post('/updatecity/:id', upload.single('photo'), (req, res) => {
+
+    const id = req.params.id
+
+    let nama = req.body.nama
+    let diresmikan = req.body.diresmikan
+    let photo = req.file.filename
+    let provinsi = req.body.provinsi
+
+    const sql = `UPDATE kabupaten_tb SET nama='${nama}', diresmikan='${diresmikan}', photo='${photo}', provinsi_id='${provinsi}' WHERE kabupaten_tb.id='${id}'`;
+
+    db.query(sql, function (err, result) {
+      if (err) throw err;
+
+      res.redirect('/')
+      })
+
+})
 
 const port = 8080
 app.listen(port, function() {
